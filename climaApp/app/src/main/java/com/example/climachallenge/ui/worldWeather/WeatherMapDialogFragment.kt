8ba5a4b562c9775a259246bcce84d6cffdfd0d30
Martina Severo <marsevero88@gmail.com>
2021-07-21 20:06:30 -0300

    Se agregan iconos a la pantalla 2 para mostrar segun condicion del clima

--- climaApp/app/src/main/java/com/example/climachallenge/ui/worldWeather/WeatherMapDialogFragment.kt
+++ climaApp/app/src/main/java/com/example/climachallenge/ui/worldWeather/WeatherMapDialogFragment.kt
@@ -11,7 +11,6 @@ import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.DialogFragment
 import com.example.climachallenge.R
-import com.example.climachallenge.retrofit.models.Daily
 import com.example.climachallenge.retrofit.models.OpenWeatherResponse
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -19,17 +18,19 @@ import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.*
 
-//FIXME setear datos para mostrar
 class WeatherMapDialogFragment(private val weatherData: OpenWeatherResponse) : DialogFragment() {
 
     private val currentWeather = weatherData.current
-    private val currentWeatherTimezone = weatherData.timezone
 
     override fun onStart() { //FIXME
         super.onStart()
-        dialog?.window?.setLayout(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setLayout(
+            ConstraintLayout.LayoutParams.MATCH_PARENT,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -45,11 +46,40 @@ class WeatherMapDialogFragment(private val weatherData: OpenWeatherResponse) : D
         val tvWindSpeed: TextView = view.findViewById(R.id.text_view_weather_map_wind_speed)
         val tvSunrise: TextView = view.findViewById(R.id.text_view_weather_map_sunrise)
         val tvSunset: TextView = view.findViewById(R.id.text_view_weather_map_sunset)
-        val tvWeatherDescription: TextView = view.findViewById(R.id.text_view_weather_map_description)
+        val tvWeatherDescription: TextView =
+            view.findViewById(R.id.text_view_weather_map_description)
         val tvWeatherIcon: ImageView = view.findViewById(R.id.image_view_weather_map_icon)
-        val tvWeatherDescriptionIcon: ImageView = view.findViewById(R.id.image_view_weather_map_description_icon)
+        //FIXME indicate which data is showing for each tv
+        tvTimezone.text = weatherData.timezone
+        tvDate.text = formatWeatherDate(currentWeather.dt)
+        tvTemperature.text = currentWeather.temp.toString()
+        tvHumidity.text = currentWeather.humidity.toString()
+        tvVisibility.text = currentWeather.visibility.toString()
+        tvWindSpeed.text = currentWeather.wind_speed.toString()
+        tvSunrise.text = currentWeather.sunrise.toString()
+        tvSunset.text = currentWeather.sunset.toString()
+        tvWeatherDescription.text = currentWeather.weather[0].description
+        tvWeatherIcon.setImageResource(getWeatherIcon())
 
         return view
+    }
+
+    private fun getWeatherIcon(): Int {
+        val weatherCondition = currentWeather.weather[0].main
+        var icon = 0
+        when (weatherCondition) {
+            "Thunderstorm" -> icon = R.drawable.ic_weather_thunderstorm
+            "Drizzle" -> icon = R.drawable.ic_weather_drizzle
+            "Rain" -> icon = R.drawable.ic_weather_rain
+            "Snow" -> icon = R.drawable.ic_weather_snow
+            "Clear" -> icon = R.drawable.ic_weather_clear
+            "Clouds" -> icon = R.drawable.ic_weather_clouds
+            else -> { //Atmosphere
+                icon = R.drawable.ic_weather_atmosphere
+            }
+        }
+
+        return icon
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
