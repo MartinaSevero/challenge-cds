3016813e9a8fc0e4f4db07f09b4b970db721a05e
Martina Severo <marsevero88@gmail.com>
2021-07-20 21:50:19 -0300

    Se maneja el estado del gps para obtener la ubicacion actual

--- climaApp/app/src/main/java/com/example/climachallenge/ui/weatherToday/WeatherTodayFragment.kt
+++ climaApp/app/src/main/java/com/example/climachallenge/ui/weatherToday/WeatherTodayFragment.kt
@@ -1,12 +1,20 @@
 package com.example.climachallenge.ui.weatherToday
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Context.LOCATION_SERVICE
+import android.content.DialogInterface
+import android.content.Intent
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,7 +23,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.climachallenge.MainActivity
 import com.example.climachallenge.R
+import com.example.climachallenge.common.MyApp
 import com.example.climachallenge.retrofit.models.OpenWeatherResponse
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -29,7 +41,7 @@ class WeatherTodayFragment : Fragment(), PermissionListener {
     private var weatherTodayViewModel: WeatherTodayViewModel? = null
     private var weatherTodayAdapter: WeatherTodayRecyclerViewAdapter? = null
     private var weatherData: OpenWeatherResponse? = null
-    //private lateinit var allPermissionsListener: PermissionListener
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private var columnCount = 1
 
@@ -62,20 +74,69 @@ class WeatherTodayFragment : Fragment(), PermissionListener {
                 adapter = weatherTodayAdapter
             }
         }
-
-        // Weather data observer
-        weatherTodayViewModel!!.getWeatherData(-34.90, -56.17)?.observe(
-            viewLifecycleOwner,
-            Observer {
-                if (it != null) {
-                    weatherData = it
-                    weatherTodayAdapter!!.setData(weatherData?.daily)
-                    (requireActivity() as MainActivity).supportActionBar?.title = resources.getString(R.string.title_today) + " - " + weatherData!!.timezone
-                }
-            })
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         checkPermissions()
 
         return view
+    }
+
+    companion object {
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            WeatherTodayFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+        if (isGpsEnabled()) {
+            fusedLocationClient.getCurrentLocation(
+                LocationRequest.PRIORITY_HIGH_ACCURACY,
+                null
+            ).addOnSuccessListener { location ->
+                if (location != null) {
+                    setAdapterData(location.latitude, location.longitude)
+                } else {
+                    Toast.makeText(
+                        MyApp.instance,
+                        MyApp.instance.getString(R.string.location_error),
+                        Toast.LENGTH_LONG
+                    ).show()
+                    setAdapterData()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(
+                    MyApp.instance,
+                    MyApp.instance.getString(R.string.location_error),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        } else {
+            Toast.makeText(
+                MyApp.instance,
+                MyApp.instance.getString(R.string.gps_error),
+                Toast.LENGTH_LONG
+            ).show()
+            setAdapterData()
+        }
+
+    }
+
+    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+        Log.i("weathertodayfragment", "Allow location permissions to show weather data")
+        setAdapterData()
+    }
+
+    override fun onPermissionRationaleShouldBeShown(
+        permission: PermissionRequest?,
+        token: PermissionToken?
+    ) {
+        Log.i("weathertodayfragment", "Turn on GPS to get current location")
     }
 
     private fun checkPermissions() {
@@ -83,9 +144,9 @@ class WeatherTodayFragment : Fragment(), PermissionListener {
             DialogOnDeniedPermissionListener.Builder.withContext(
                 activity
             )
-                .withTitle("title")
-                .withMessage("message")
-                .withButtonText("button text")
+                .withTitle(R.string.denied_location_title)
+                .withMessage(R.string.denied_location_message)
+                .withButtonText(R.string.denied_location_button)
                 .withIcon(R.mipmap.ic_launcher)
                 .build()
 
@@ -99,31 +160,31 @@ class WeatherTodayFragment : Fragment(), PermissionListener {
             .check()
     }
 
-    companion object {
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-                WeatherTodayFragment().apply {
-                    arguments = Bundle().apply {
-                        putInt(ARG_COLUMN_COUNT, columnCount)
-                    }
+    private fun setAdapterData(latitude: Double = -34.90, longitude: Double = -56.17) {
+        // Weather data observer
+        weatherTodayViewModel!!.getWeatherData(latitude, longitude)?.observe(
+            viewLifecycleOwner,
+            Observer {
+                if (it != null) {
+                    weatherData = it
+                    weatherTodayAdapter!!.setData(weatherData?.daily)
+                    (requireActivity() as MainActivity).supportActionBar?.title =
+                        resources.getString(
+                            R.string.title_today
+                        ) + " - " + weatherData!!.timezone
                 }
+            })
     }
 
-    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
-        Log.i("weathertodayfragment", "onPermissionGranted not implemented")
+    private fun isGpsEnabled(): Boolean {
+        var gpsEnabled = false
+        val locationManager =
+            requireContext().getSystemService(LOCATION_SERVICE) as LocationManager
+        try {
+            gpsEnabled =
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) //GPS is enabled
+        } catch (ex: Exception) {
+        }
+        return gpsEnabled
     }
-
-    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
-        Log.i("weathertodayfragment", "Allow location permissions to receive data")
-    }
-
-    override fun onPermissionRationaleShouldBeShown(
-        permission: PermissionRequest?,
-        token: PermissionToken?
-    ) {
-        Log.i("weathertodayfragment", "onPermissionRationaleShouldBeShown not implemented")
-    }
-
 }
